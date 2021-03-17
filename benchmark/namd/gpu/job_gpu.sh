858d1affd240aaaf7fddb8771dcb3ff07d4df04c
pojeda <pedroojeda2011@gmail.com>
2021-03-17 18:18:04 +0100

    Namd mts test case

--- benchmark/namd/gpu/job_gpu.sh
+++ benchmark/namd/gpu/job_gpu.sh
@@ -15,6 +15,7 @@ ml purge  > /dev/null 2>&1
 ml GCC/5.4.0-2.26  CUDA/8.0.61_375.26  OpenMPI/2.0.2
 ml NAMD/2.12-nompi
 
-namd2 +p28 +setcpuaffinity +idlepoll +devices $CUDA_VISIBLE_DEVICES step4_equilibration.inp > output_prod.dat
+namd2 +p28 +setcpuaffinity +idlepoll +devices $CUDA_VISIBLE_DEVICES step4_equilibration.inp > output_gpu1.dat
+namd2 +p28 +setcpuaffinity +idlepoll +devices $CUDA_VISIBLE_DEVICES step4_equilibration_mts.inp > output_gpu2.dat
 
 exit 0
--- benchmark/namd/job_mpi.sh
+++ benchmark/namd/job_mpi.sh
@@ -11,11 +11,12 @@ module add GCC/6.3.0-2.27  OpenMPI/2.0.2
 module add NAMD/2.12-mpi
 
 srun namd2 step4_equilibration.inp > output_mpi1.dat
+srun namd2 step4_equilibration_mts.inp > output_mpi2.dat
 
 #Load modules necessary for running NAMD
 ml purge  > /dev/null 2>&1 
 ml GCC/7.3.0-2.30  OpenMPI/3.1.1
 ml NAMD/2.13-mpi
 
-srun namd2 step4_equilibration.inp > output_mpi2.dat
+srun namd2 step4_equilibration.inp > output_mpi3.dat
 
