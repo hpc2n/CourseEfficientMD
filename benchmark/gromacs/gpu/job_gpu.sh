3a6276584c982b9e206304a8d30bed41896b1bd4
pojeda <pedroojeda2011@gmail.com>
2021-04-05 13:58:01 +0200

    Gromacs job_gpu unified

---
+++ benchmark/gromacs/gpu/job_gpu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH -A Project_ID
+#SBATCH -t 00:20:00
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH -c 7
+#K80 cards
+#SBATCH --gres=gpu:k80:2
+#V100 cards
+##SBATCH --gres=gpu:v100:2
+#SBATCH --output=job_str.out
+#SBATCH --error=job_str.err
+#SBATCH --exclusive
+
+ml purge  > /dev/null 2>&1 
+ml GCC/8.3.0  CUDA/10.1.243  OpenMPI/3.1.4
+ml GROMACS/2021-Python-3.7.4
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export reset_counters="-resetstep 10000 -nsteps 20000"
+
+gmx grompp -f step4.1_equilibration.mdp -o step4.1_equilibration.tpr -c step4.0_minimization.gro -r step3_charmm2gmx.pdb -n index.ndx -p topol.top
+
+#Running with default options (MPI-Threaded version)
+gmx mdrun -ntomp $SLURM_CPUS_PER_TASK -ntmpi $SLURM_NTASKS $reset_counters -dlb yes  -v -deffnm step4.1_equilibration
+
+#Using 1 rank for PME and offloading NB and PME to the GPUs (MPI-Threaded version)
+gmx mdrun -nb gpu -pme gpu -npme 1 -ntomp $SLURM_CPUS_PER_TASK -ntmpi $SLURM_NTASKS $reset_counters -dlb yes  -v -deffnm step4.1_equilibration
+
+#Running with default options (MPI version)
+srun gmx_mpi mdrun -ntomp $SLURM_CPUS_PER_TASK $reset_counters -dlb yes  -v -deffnm step4.1_equilibration
+
+#Using 1 rank for PME and offloading NB and PME to the GPUs (MPI version)
+srun gmx_mpi mdrun -nb gpu -pme gpu -npme 1 -ntomp $SLURM_CPUS_PER_TASK  $reset_counters -dlb yes  -v -deffnm step4.1_equilibration
+
+exit 0
--- benchmark/gromacs/gpu/job_gpu_skylake_offload.sh
+++ benchmark/gromacs/gpu/job_gpu_skylake_offload.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
-#SBATCH -A SNIC2020-9-25
-#SBATCH -t 00:05:00
+#SBATCH -A staff
+#SBATCH -t 00:10:00
 #SBATCH -N 1
 #SBATCH -n 4
 #SBATCH -c 7
-#SBATCH --constraint=skylake
 #SBATCH --gres=gpu:v100:2
 #SBATCH --output=job_str.out
 #SBATCH --error=job_str.err
 #SBATCH --mail-type=END
 #SBATCH --exclusive
 
-ml purge
-
-#ml GCC/6.4.0-2.28  CUDA/9.0.176  impi/2017.3.196
-#ml GROMACS/2018
-#.1
-#ml GCC/7.3.0-2.30  CUDA/9.2.88  OpenMPI/3.1.1
-#ml GROMACS/2018.3 
 
 
-#ml GCC/5.4.0-2.26  CUDA/8.0.61_375.26  impi/2017.3.196
-#ml GROMACS/2016.3-PLUMED 
-
-ml GCC/8.2.0-2.31.1  CUDA/10.1.105  OpenMPI/3.1.3
-ml GROMACS/2019.2
-
-if [ -n "$SLURM_CPUS_PER_TASK" ]; then
-    mdargs="-ntomp $SLURM_CPUS_PER_TASK"
-else
-    mdargs="-ntomp 1"
-fi
-
-#nvidia-smi -q
+ml purge  > /dev/null 2>&1 
+ml GCC/8.3.0  CUDA/10.1.243  OpenMPI/3.1.4
+ml GROMACS/2021-Python-3.7.4
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 gmx grompp -f step4.1_equilibration.mdp -o step4.1_equilibration.tpr -c step4.0_minimization.gro -r step3_charmm2gmx.pdb -n index.ndx -p topol.top
+gmx mdrun -nb gpu -pme gpu -npme 1 -ntomp 7 -ntmpi $SLURM_NTASKS -resetstep 10000 -nsteps 20000 -dlb yes  -v -deffnm step4.1_equilibration
+srun gmx_mpi mdrun -nb gpu -pme gpu -npme 1 -ntomp 7  -resetstep 10000 -nsteps 10000 -dlb yes  -v -deffnm step4.1_equilibration
 
-
-gmx mdrun -gputasks 0123 -nb gpu -pme gpu -npme 1 -ntomp 7 -ntmpi $SLURM_NTASKS -dlb yes  -v -deffnm step4.1_equilibration
 
 
 
