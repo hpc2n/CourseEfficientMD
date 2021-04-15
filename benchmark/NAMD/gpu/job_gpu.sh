42eaf7968355be604c47cb8aaca326ab584583ea
pojeda <pedroojeda2011@gmail.com>
2021-04-15 13:56:18 +0200

    NAMD GPU example cleaned

--- benchmark/NAMD/gpu/job_gpu.sh
+++ benchmark/NAMD/gpu/job_gpu.sh
@@ -5,9 +5,9 @@
 #SBATCH -N 1
 #SBATCH -n 28
 #SBATCH --gres=gpu:k80:2
+#For V100 uncomment the following line and comment the line above
+##SBATCH --gres=gpu:v100:2
 #SBATCH --exclusive
-
-echo $CUDA_VISIBLE_DEVICES
 
 
 #Load modules necessary for running NAMD
@@ -18,4 +18,3 @@ ml NAMD/2.12-nompi
 namd2 +p28 +setcpuaffinity +idlepoll +devices $CUDA_VISIBLE_DEVICES step4_equilibration.inp > output_gpu1.dat
 namd2 +p28 +setcpuaffinity +idlepoll +devices $CUDA_VISIBLE_DEVICES step4_equilibration_mts.inp > output_gpu2.dat
 
-exit 0
