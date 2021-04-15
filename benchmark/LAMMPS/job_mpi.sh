757c072e7856f45381c301d87027282edfa434ab
pojeda <pedroojeda2011@gmail.com>
2021-04-15 20:49:15 +0200

    LAMMPS MPI and OMP examples

--- benchmark/LAMMPS/job_mpi.sh
+++ benchmark/LAMMPS/job_mpi.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH -A SNICyyyy-xx-yy
 #SBATCH -t 02:10:00
-#SBATCH -N 1
 #SBATCH -n 28
 
 
 #Load modules necessary for running LAMMPS
-module load icc/2017.1.132-GCC-6.3.0-2.27 ifort/2017.1.132-GCC-6.3.0-2.27  impi/2017.1.132
-module load LAMMPS/31Mar17
+ml GCC/8.3.0  OpenMPI/3.1.4
+ml LAMMPS/3Mar2020-Python-3.7.4-kokkos
 
 #Execute LAMMPS
-#srun lmp_intel_cpu -in step4.0_minimization.inp 
-srun lmp_intel_cpu -in step4.1_equilibration.inp
+#srun lmp -in step4.0_minimization.inp 
+srun lmp -in step4.1_equilibration.inp
--- benchmark/LAMMPS/job_omp.sh
+++ benchmark/LAMMPS/job_omp.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #SBATCH -A SNICyyyy-xx-yy
 #SBATCH -t 02:10:00
-#SBATCH -N 1
 #SBATCH -n 7
 #SBATCH -c 4
 
 #Load modules necessary for running LAMMPS
-module load icc/2017.1.132-GCC-6.3.0-2.27 ifort/2017.1.132-GCC-6.3.0-2.27  impi/2017.1.132
-module load LAMMPS/31Mar17
+ml GCC/8.3.0  OpenMPI/3.1.4
+ml LAMMPS/3Mar2020-Python-3.7.4-kokkos
 
 export OMP_NUM_THREADS=4
 
 #Execute LAMMPS
-#srun lmp_intel_cpu -in step4.0_minimization.inp 
-srun lmp_intel_cpu -in step4.1_equilibration.inp
+#srun lmp -in step4.0_minimization.inp 
+srun lmp -in step4.1_equilibration.inp
